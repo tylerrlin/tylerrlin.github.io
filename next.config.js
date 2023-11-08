e1cecc3d12b45d4705f7c560968351e31377222d
tylerrlin <tylerrlin@gmail.com>
2023-11-08 11:35:29 -0500

    config fix

--- next.config.js
+++ next.config.js
@@ -1,12 +1,7 @@
 /** @type {import('next').NextConfig} */
 
-if (process.env.NODE_ENV == "development") {
-    const nextConfig = {};
-    module.exports = nextConfig;
-} else {
-    const nextConfig = {
-        trailingSlash: true,
-        output: "export",
-    };
-    module.exports = nextConfig;
-}
+const nextConfig = {
+    trailingSlash: true,
+    output: "export",
+};
+module.exports = nextConfig;
