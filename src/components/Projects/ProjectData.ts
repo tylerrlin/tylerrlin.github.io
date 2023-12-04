4ab6dfde45e7b93b8d8589575196d554cff72fac
tylerrlin <tylerrlin@gmail.com>
2023-12-03 20:26:24 -0500

    update comprchess image

--- src/components/Projects/ProjectData.ts
+++ src/components/Projects/ProjectData.ts
@@ -12,10 +12,19 @@ const ProjectData: ProjectDataProps[] = [
         image: "/comprchess.png",
         title: "COMPR[CH]ESS",
         description:
-            "Lossless chess compression algorithm that adaptively and efficiently encodes chess games at given rating range.",
+            "Lossless chess compression algorithm that adaptively and efficiently encodes chess games at a given rating range.",
         github: "https://github.com/tylerrlin/compr-ch-ess",
         deployed: "/projects/comprchess",
         skills: ["Python", "Tensorflow", "Pandas"],
+    },
+    {
+        image: "/project1.png",
+        title: "Personal Portfolio",
+        description:
+            "Personal portfolio built with NextJS 13. Custom animations utilizing Framer Motion and responsive app optimized for mobile/desktop. Focus on clean design and user experience.",
+        github: "https://github.com/tylerrlin/tylerrlin.github.io",
+        deployed: "https://tylerrlin.github.io",
+        skills: ["NextJS 13", "React", "TypeScript", "TailwindCSS"],
     },
 ];
 
