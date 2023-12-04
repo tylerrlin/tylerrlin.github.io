c206867fd7f1766f9c29b59512b728fc37729e63
tylerrlin <tylerrlin@gmail.com>
2023-12-03 20:36:39 -0500

    updates to comprchess page

--- src/app/projects/comprchess/page.tsx
+++ src/app/projects/comprchess/page.tsx
@@ -31,15 +31,22 @@ const ChessCompression = () => {
                 This program utilizes Huffman Coding to encode/decode chess
                 games. Huffman Coding is a technique where more probable
                 characters or strings of characters are assigned lower-length,
-                unique bitstrings. Using a chess engine to evaluate positions
-                and a machine learning model trained to predict a player&apos;s
-                next move, every unique move is encoded and the resulting
-                move&apos;s code is appended to a bitstring containing the code
-                of the whole game. By training models on a large dataset of a
-                specified rating range, the program can encode and decode games
-                more efficiently in that rating range. Using this technique, an
-                average compression rate of 85% was achieved on a dataset of
-                1,000 games from lichess.org.
+                unique bitstrings. In this context, more probable chess moves
+                are given shorter bitstrings.
+                <br />
+                <br />A machine learning model is used to predict the next move
+                in a chess game. Models are trained on a dataset of thousands of
+                chess games from lichess, filtered to only include games from
+                players with average ratings inside a certain range. As a
+                result, the model is able to accurately predict the next move
+                from a given position based on the player&apos;s rating.
+                <br />
+                <br />
+                From probabilities given by the model, Huffman Coding is used to
+                adaptively encode/decode chess games into binary strings. Due to
+                the model being trained on games from players with similar
+                ratings, the codes generated accurately reflect the probability
+                of a move being played in a given position.
             </h2>
             <Link
                 href="https://github.com/tylerrlin/compr-ch-ess"
